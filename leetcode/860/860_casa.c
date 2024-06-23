920ab484db6486081070cf6058d180883d80bcec
ganassini <guiganassini17@gmail.com>
2024-06-23 17:40:58 -0300

    doom3 code style

--- leetcode/860/860_casa.c
+++ leetcode/860/860_casa.c
@@ -1,50 +1,37 @@
 bool lemonadeChange(int* bills, int billsSize) 
 {
-	struct cashRegister 
-	{
+	struct cashRegister_t {
 		int fiveBills;
 		int tenBills;
 	};
-	
-	struct cashRegister cashRegister = {0, 0};
+    
+	struct cashRegister_t cashRegister = {0, 0};
 
 	int i;
 	
-	if (bills[0] > 5)
-	{
+	if ( bills[0] > 5 ) {
 		return false;
 	}
 
 	cashRegister.fiveBills += 1;
 
-	for (i=1; i<billsSize; i++) 
-	{
-		if (bills[i] == 5) 
-		{
+	for ( i = 1; i < billsSize; i++ ) {
+		if ( bills[i] == 5 ) {
 			cashRegister.fiveBills += 1;
 		}
-		if (bills[i] == 10) 
-		{
-			if (cashRegister.fiveBills == 0) 
-			{
+		if ( bills[i] == 10 ) {
+			if ( cashRegister.fiveBills == 0 ) {
 				return false;
 			}
 			cashRegister.fiveBills -= 1;
 			cashRegister.tenBills += 1;
 		}
-		if (bills[i] == 20) 
-		{
-			/* 
-			 * [5,5,10,5,20,5,5,5,5,5,20,5,10,5,5,5,5,20,20,5] 
-			 */
-			if (cashRegister.fiveBills < 1 )
-			{
+		if ( bills[i] == 20 ) {
+			if ( cashRegister.fiveBills < 1 ) {
 				return false;
 			}
-			if (cashRegister.tenBills < 1) 
-			{
-				if (cashRegister.fiveBills < 3) 
-				{
+			if ( cashRegister.tenBills < 1 ) {
+				if ( cashRegister.fiveBills < 3 ) {
 					return false;
 				}
 				cashRegister.fiveBills -= 3;
